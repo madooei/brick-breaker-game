f816d800c942738e4b78f34bcad213521fd22381
Ali Madooei <madooei@jhu.edu>
2026-02-08 18:58:25 -0500

    brick-02: Animate the ball with requestAnimationFrame

--- src/main.js
+++ src/main.js
@@ -8,3 +8,23 @@ ctx.rect(20, 40, 50, 50); // x, y, width, height
 ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
+
+let x = canvas.width / 2
+let y = canvas.height - 30;
+const dx = 2;
+const dy = -2;
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.rect(x, y, 10, 10);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+  x += dx;
+  y += dy;
+
+  window.requestAnimationFrame(draw);
+}
+
+draw();
