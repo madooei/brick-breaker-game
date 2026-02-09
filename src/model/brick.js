f33970bef6932d2c33afc51b346f015c07fd8bab
Ali Madooei <madooei@jhu.edu>
2026-02-08 22:15:00 -0500

    brick-13: Create Brick class with visibility toggling

---
+++ src/model/brick.js
@@ -0,0 +1,23 @@
+import Block from "./block.js";
+
+class Brick extends Block {
+  constructor(x, y, width, height, color) {
+    super(x, y, width, height, color);
+    this.visible = true;
+  }
+
+  draw(ctx) {
+    if (this.visible) {
+      super.draw(ctx);
+    }
+  }
+
+  collides(ball) {
+    if (this.visible && this.intersects(ball)) {
+      this.visible = false;
+      ball.collides(this); // causes the ball to bounce off
+    }
+  }
+}
+
+export default Brick;
